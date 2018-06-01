047ae296d429f27ca2e1e9c9e4ccc35bbd1d8237
Udit Narayan Sia <udit.sia@gmail.com>
2018-06-02 02:11:24 +0530

    style overhaul

--- place_auto_and_direction.js
+++ place_auto_and_direction.js
@@ -16,6 +16,8 @@ function getLocation() {
 }
 function showPosition(position) {
     GetRoute();
+   // document.querySelector("#txtDestination").addEventListener("mouseout", GetRoute);
+    document.querySelector("#getDirection").addEventListener("click", GetRoute);
     function GetRoute() {
         var uluru = {lat: position.coords.latitude, lng: position.coords.longitude};
         var mapOptions = {
@@ -25,44 +27,45 @@ function showPosition(position) {
         map = new google.maps.Map(document.getElementById('dvMap'), mapOptions);
         directionsDisplay.setMap(map);
         directionsDisplay.setPanel(document.getElementById('dvPanel'));
+        /*********DIRECTIONS AND ROUTE**********************/
+        source = document.getElementById("txtSource").value;
+        destination = document.getElementById("txtDestination").value;
+
+        var request = {
+            origin: source,
+            destination: destination,
+            travelMode: google.maps.TravelMode.DRIVING
+        };
+        directionsService.route(request, function (response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+            }
+        });
+
+        /*********DISTANCE AND DURATION**********************/
+        var service = new google.maps.DistanceMatrixService();
+        service.getDistanceMatrix({
+            origins: [source],
+            destinations: [destination],
+            travelMode: google.maps.TravelMode.DRIVING,
+            unitSystem: google.maps.UnitSystem.METRIC,
+            avoidHighways: false,
+            avoidTolls: false
+        }, function (response, status) {
+            if (status == google.maps.DistanceMatrixStatus.OK && response.rows[0].elements[0].status != "ZERO_RESULTS") {
+                var distance = response.rows[0].elements[0].distance.text;
+                var duration = response.rows[0].elements[0].duration.text;
+                var dvDistance = document.getElementById("dvDistance");
+                dvDistance.innerHTML = "";
+                dvDistance.innerHTML += "Distance: " + distance + "<br />";
+                dvDistance.innerHTML += "Duration:" + duration;
+
+            } else {
+                alert("Unable to find the distance via road.");
+            }
+        });
+    }
 }
 
 
-    /*********DIRECTIONS AND ROUTE**********************/
-    source = document.getElementById("txtSource").value;
-    destination = document.getElementById("txtDestination").value;
 
-    var request = {
-        origin: source,
-        destination: destination,
-        travelMode: google.maps.TravelMode.DRIVING
-    };
-    directionsService.route(request, function (response, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(response);
-        }
-    });
-
-    /*********DISTANCE AND DURATION**********************/
-    var service = new google.maps.DistanceMatrixService();
-    service.getDistanceMatrix({
-        origins: [source],
-        destinations: [destination],
-        travelMode: google.maps.TravelMode.DRIVING,
-        unitSystem: google.maps.UnitSystem.METRIC,
-        avoidHighways: false,
-        avoidTolls: false
-    }, function (response, status) {
-        if (status == google.maps.DistanceMatrixStatus.OK && response.rows[0].elements[0].status != "ZERO_RESULTS") {
-            var distance = response.rows[0].elements[0].distance.text;
-            var duration = response.rows[0].elements[0].duration.text;
-            var dvDistance = document.getElementById("dvDistance");
-           dvDistance.innerHTML = "";
-            dvDistance.innerHTML += "Distance: " + distance + "<br />";
-            dvDistance.innerHTML += "Duration:" + duration;
-
-        } else {
-            alert("Unable to find the distance via road.");
-        }
-    });
-}
